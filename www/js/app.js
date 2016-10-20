47688b68c816259747d6333c8decdf552b8fda6c
Jonathon Hawkins <jonathon.hawkins@conkergroup.com>
2016-10-20 13:17:00 +0100

    Moved firebase logic into controler, updated for with ionic elements, and switched out previouse logic/event handlers for angular(ng) ones, along with adding some examples of  ng html functions (ng-if / model / click / disabled / bind-html )

--- www/js/app.js
+++ www/js/app.js
@@ -5,163 +5,109 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-//angular.module('starter', ['ionic', 'starter.controllers', 'starter.services']) old one
+angular.module( 'starter', [ 'ionic', 'starter.controllers', 'starter.services' ] )
 
-angular.module('starter', ['ionic', 'firebase', 'ngCordova'])
-
-
-//sign up
-$scope.signupEmail = function(){  
- 
-  var ref = new Firebase("https://mobile-app-uni.firebaseio.com");
- 
-  ref.createUser({
-    email    : $scope.data.email,
-    password : $scope.data.password
-  }, function(error, userData) {
-    if (error) {
-      console.log("Error creating user:", error);
-    } else {
-      console.log("Successfully created user account with uid:", userData.uid);
-    }
-  });
- 
-};
-
-
-//login
-
-$scope.loginEmail = function(){
- 
-  var ref = new Firebase("https://mobile-app-uni.firebaseio.com");
- 
-  ref.authWithPassword({
-    email    : $scope.data.email,
-    password : $scope.data.password
-  }, function(error, authData) {
-    if (error) {
-      console.log("Login Failed!", error);
-    } else {
-      console.log("Authenticated successfully with payload:", authData);
-    }
-  });
- 
-};
-
-//facebook
-$scope.loginFacebook = function(){
- 
-  var ref = new Firebase("https://mobile-app-uni.firebaseio.com");
- 
- 
-  if(ionic.Platform.isWebView()){
- 
-    $cordovaFacebook.login(["public_profile", "email"]).then(function(success){
- 
-      console.log(success);
- 
-      ref.authWithOAuthToken("facebook", success.authResponse.accessToken, function(error, authData) {
-        if (error) {
-          console.log('Firebase login failed!', error);
-        } else {
-          console.log('Authenticated successfully with payload:', authData);
-        }
-      });
- 
-    }, function(error){
-      console.log(error);
-    });        
- 
-  }
-  else {
- 
-    ref.authWithOAuthPopup("facebook", function(error, authData) {
-      if (error) {
-        console.log("Login Failed!", error);
-      } else {
-        console.log("Authenticated successfully with payload:", authData);
-      }
-    });
- 
-  }
- 
-};
-
-
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
+.run( function ( $ionicPlatform )
+{
+  $ionicPlatform.ready( function ( )
+  {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    if ( window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard )
+    {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar( true );
+      cordova.plugins.Keyboard.disableScroll( true );
 
     }
-    if (window.StatusBar) {
+    if ( window.StatusBar )
+    {
       // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
+      StatusBar.styleDefault( );
     }
-  });
-})
+  } );
+} )
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config( function ( $stateProvider, $urlRouterProvider )
+{
 
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
   $stateProvider
+    .state( 'login',
+    {
+      url: '/login',
+      templateUrl: 'templates/login.html',
+      controller: 'LoginCtrl'
+    } )
+
+
+
 
   // setup an abstract state for the tabs directive
-    .state('tab', {
+  .state( 'tab',
+  {
     url: '/tab',
     abstract: true,
     templateUrl: 'templates/tabs.html'
-  })
+  } )
 
   // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
+  .state( 'tab.dash',
+  {
     url: '/dash',
-    views: {
-      'tab-dash': {
+    views:
+    {
+      'tab-dash':
+      {
         templateUrl: 'templates/tab-dash.html',
         controller: 'DashCtrl'
       }
     }
-  })
+  } )
 
-  .state('tab.chats', {
+  .state( 'tab.chats',
+    {
       url: '/chats',
-      views: {
-        'tab-chats': {
+      views:
+      {
+        'tab-chats':
+        {
           templateUrl: 'templates/tab-chats.html',
           controller: 'ChatsCtrl'
         }
       }
-    })
-    .state('tab.chat-detail', {
+    } )
+    .state( 'tab.chat-detail',
+    {
       url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
+      views:
+      {
+        'tab-chats':
+        {
           templateUrl: 'templates/chat-detail.html',
           controller: 'ChatDetailCtrl'
         }
       }
-    })
+    } )
 
-  .state('tab.account', {
+  .state( 'tab.account',
+  {
     url: '/account',
-    views: {
-      'tab-account': {
+    views:
+    {
+      'tab-account':
+      {
         templateUrl: 'templates/tab-account.html',
         controller: 'AccountCtrl'
       }
     }
-  });
+  } );
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise( '/login' );
 
-});
+} );
+
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,352 @@
-angular.module('starter.controllers', [])
+app = angular.module( 'starter.controllers', [ ] )
 
-.controller('DashCtrl', function($scope) {})
+app.controller( 'DashCtrl', function ( $scope, Firebase ) {} )
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+syntaxHighlight = function ( json )
+{
+	if ( typeof json != 'string' )
+	{
+		json = JSON.stringify( json, null, 4 );
+	}
+	json = json.replace( /&/g, '&amp;' ).replace( /</g, '&lt;' ).replace( />/g, '&gt;' );
+	return json.replace( /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function ( match )
+	{
+		var cls = 'number';
+		if ( /^"/.test( match ) )
+		{
+			if ( /:$/.test( match ) )
+			{
+				cls = 'key';
+			}
+			else
+			{
+				cls = 'string';
+			}
+		}
+		else if ( /true|false/.test( match ) )
+		{
+			cls = 'boolean';
+		}
+		else if ( Number.isInteger( match ) )
+		{
+			console.log( "WE FOUND AN INT!" )
+			cls = 'int';
+		}
+		else if ( /null/.test( match ) )
+		{
+			cls = 'null';
+		}
+		return '<span class="' + cls + '">' + match + '</span>';
+	} );
+}
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+pretty = function ( json, heavy )
+{
+	if ( heavy )
+	{
+		return syntaxHighlight( json )
+	}
+	return JSON.stringify( json, null, 4 );
+};
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+app.controller( 'LoginCtrl', function ( $scope )
+{
+	// Initialize Firebase
+	var config = {
+		apiKey: "AIzaSyDXuQU3J2jRIYFf2ZrUfAU3yId5O9EfMzQ",
+		authDomain: "mobile-app-uni.firebaseapp.com",
+		databaseURL: "https://mobile-app-uni.firebaseio.com",
+		storageBucket: "mobile-app-uni.appspot.com",
+		messagingSenderId: "968206557542"
+	};
+	// JH !!!important!!!-  stupid angular scoping issues means that the ion-content 
+	// container makes a clone of the $scope meaning you need to decalre vars ahead of
+	// using them in the view otherwise the js wont know what you are refferencing
+	$scope.auth = {
+		email: "",
+		password: ""
+	}
+	$scope.error_output = {
+		user: "",
+		admin: ""
+	}
+	$scope.user_data = {
+		displayName: "",
+		email: "",
+		emailVerified: "",
+		photoURL: "",
+		isAnonymous: "",
+		uid: "",
+		providerData: ""
+	}
+	firebase.initializeApp( config );
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+	/**
+	 * Handles the sign in button press.
+	 */
+	$scope.toggleSignIn = function ( )
+	{
+		if ( firebase.auth( ).currentUser )
+		{
+			// [START signout]
+			firebase.auth( ).signOut( );
+			// [END signout]
+		}
+		else
+		{
+			var email = $scope.auth.email;
+			var password = $scope.auth.password;
+			if ( !email || email.length < 4 )
+			{
+				$scope.error_output.user = 'Please enter an email address.';
+				return;
+			}
+			if ( !password || password.length < 4 )
+			{
+				$scope.error_output.user = 'Please enter a password.';
+				return;
+			}
+			// Sign in with email and pass.
+			// [START authwithemail]
+			firebase.auth( ).signInWithEmailAndPassword( email, password ).catch( function ( error )
+			{
+				// Handle Errors here.
+				var errorCode = error.code;
+				var errorMessage = error.message;
+				// [START_EXCLUDE]
+				if ( errorCode === 'auth/wrong-password' )
+				{
+					$scope.error_output.user = "Wrong password."
+				}
+				else
+				{
+					$scope.error_output.admin = "Wrong password."
+					console.error( errorMessage );
+				}
+				console.log( error );
+
+				$scope.signinable = false
+					// document.getElementById( 'quickstart-sign-in' ).disabled = false;
+					// [END_EXCLUDE]
+			} );
+			// [END authwithemail]
+		}
+		$scope.signinable = true
+			// document.getElementById( 'quickstart-sign-in' ).disabled = true;
+	}
+
+	/**
+	 * Handles the sign up button press.
+	 */
+	$scope.handleSignUp = function ( )
+	{
+		var email = $scope.auth.email;
+		var password = $scope.auth.password;
+		if ( email.length < 4 )
+		{
+			console.info( 'Please enter an email address.' );
+			return;
+		}
+		if ( password.length < 4 )
+		{
+			console.info( 'Please enter a password.' );
+			return;
+		}
+		// Sign in with email and pass.
+		// [START createwithemail]
+		firebase.auth( ).createUserWithEmailAndPassword( email, password ).catch( function ( error )
+		{
+			// Handle Errors here.
+			var errorCode = error.code;
+			var errorMessage = error.message;
+			// [START_EXCLUDE]
+			if ( errorCode == 'auth/weak-password' )
+			{
+				console.error( 'The password is too weak.' );
+			}
+			else
+			{
+				console.error( errorMessage );
+			}
+			console.log( error );
+			// [END_EXCLUDE]
+		} );
+		// [END createwithemail]
+	}
+
+	/**
+	 * Sends an email verification to the user.
+	 */
+	$scope.sendEmailVerification = function ( )
+	{
+		// [START sendemailverification]
+		firebase.auth( ).currentUser.sendEmailVerification( ).then( function ( )
+		{
+			// Email Verification sent!
+			// [START_EXCLUDE]
+			console.info( 'Email Verification Sent!' );
+			// [END_EXCLUDE]
+		} );
+		// [END sendemailverification]
+	}
+
+	$scope.sendPasswordReset = function ( )
+		{
+			var email = $scope.auth.email;
+			// var email = document.getElementById( 'email' ).value;
+			// [START sendpasswordemail]
+			firebase.auth( ).sendPasswordResetEmail( email ).then( function ( )
+			{
+				// Password Reset Email Sent!
+				// [START_EXCLUDE]
+				console.info( 'Password Reset Email Sent!' );
+				// [END_EXCLUDE]
+			} ).catch( function ( error )
+			{
+				// Handle Errors here.
+				var errorCode = error.code;
+				var errorMessage = error.message;
+				// [START_EXCLUDE]
+				if ( errorCode == 'auth/invalid-email' )
+				{
+					console.error( errorMessage );
+				}
+				else if ( errorCode == 'auth/user-not-found' )
+				{
+					console.error( errorMessage );
+				}
+				console.log( error );
+				// [END_EXCLUDE]
+			} );
+			// [END sendpasswordemail];
+		}
+		// firebase.auth( ).onAuthStateChanged( function ( user )
+		// 	{
+		// 		console.log( "AUTH CHANGE", user )
+		// 	} )
+
+
+	/**
+	 * initApp handles setting up UI event listeners and registering Firebase auth listeners:
+	 *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
+	 *    out, and that is where we update the UI.
+	 */
+	$scope.initApp = function ( )
+	{
+		// Listening for auth state changes.[START authstatelistener]
+		firebase.auth( ).onAuthStateChanged( function ( user )
+		{
+			console.log( "AUTH CHANGE", user )
+				// [START_EXCLUDE silent]
+				// document.getElementById('quickstart-verify-email').disabled = true;
+			$scope.verifyable = true
+				// [END_EXCLUDE]
+			if ( user )
+			{
+				// User is signed in.
+				$scope.signed_in = true;
+				var displayName = user.displayName;
+				var email = user.email;
+				var emailVerified = user.emailVerified;
+				var photoURL = user.photoURL;
+				var isAnonymous = user.isAnonymous;
+				var uid = user.uid;
+				var providerData = user.providerData;
+				$scope.user_data.displayName = displayName
+				$scope.user_data.email = email
+				$scope.user_data.emailVerified = emailVerified
+				$scope.user_data.photoURL = photoURL
+				$scope.user_data.isAnonymous = isAnonymous
+				$scope.user_data.uid = uid
+				$scope.user_data.providerData = providerData
+					// [START_EXCLUDE silent]
+				$scope.login_status = 'Signed in'
+				$scope.sign_status_text = 'Sign out'
+				$scope.quick_start_acc_details = pretty( user )
+				console.log( "view should have changed with this data - ", $scope.user_data )
+
+				// document.getElementById( 'quickstart-sign-in-status' ).textContent = 'Signed in';
+				// document.getElementById( 'quickstart-sign-in' ).textContent = 'Sign out';
+				// document.getElementById( 'quickstart-account-details' ).textContent = JSON.stringify( user, null, '  ' );
+				if ( !emailVerified )
+				{
+
+					$scope.verifyable = false
+						// document.getElementById( 'quickstart-verify-email' ).disabled = false;
+				}
+				// [END_EXCLUDE]
+			}
+			else
+			{
+				// User is signed out.
+				// [START_EXCLUDE silent]
+				$scope.user_data.displayName = ""
+				$scope.user_data.email = ""
+				$scope.user_data.emailVerified = ""
+				$scope.user_data.photoURL = ""
+				$scope.user_data.isAnonymous = ""
+				$scope.user_data.uid = ""
+				$scope.user_data.providerData = ""
+				$scope.signed_in = false;
+				$scope.login_status = 'Signed out'
+				$scope.sign_status_text = 'Sign in'
+				$scope.quick_start_acc_details = 'null'
+				console.log( "view should have changed with this data - ", $scope.user_data )
+					// document.getElementById( 'quickstart-sign-in-status' ).textContent = 'Signed out';
+					// document.getElementById( 'quickstart-sign-in' ).textContent = 'Sign in';
+					// document.getElementById( 'quickstart-account-details' ).textContent = 'null';
+					// [END_EXCLUDE]
+			}
+			// [START_EXCLUDE silent]
+			// document.getElementById( 'quickstart-sign-in' ).disabled = false;
+			$scope.signinable = false
+				// [END_EXCLUDE]
+			$scope.$apply( )
+		} );
+		// [END authstatelistener]
+
+		// document.getElementById( 'quickstart-sign-in' ).addEventListener( 'click', toggleSignIn, false );
+		// document.getElementById( 'quickstart-sign-up' ).addEventListener( 'click', handleSignUp, false );
+		// document.getElementById( 'quickstart-verify-email' ).addEventListener( 'click', sendEmailVerification, false );
+		// document.getElementById( 'quickstart-password-reset' ).addEventListener( 'click', sendPasswordReset, false );
+	}
+
+	// window.onload = function ( )
+	// {
+	$scope.initApp( );
+	// };
+
+
+} )
+
+
+
+app.controller( 'ChatsCtrl', function ( $scope, Chats )
+{
+	// With the new view caching in Ionic, Controllers are only called
+	// when they are recreated or on app start, instead of every page change.
+	// To listen for when this page is active (for example, to refresh data),
+	// listen for the $ionicView.enter event:
+	//
+	//$scope.$on('$ionicView.enter', function(e) {
+	//});
+
+	$scope.chats = Chats.all( );
+	$scope.remove = function ( chat )
+	{
+		Chats.remove( chat );
+	};
+} )
+
+app.controller( 'ChatDetailCtrl', function ( $scope, $stateParams, Chats )
+{
+	$scope.chat = Chats.get( $stateParams.chatId );
+} )
+
+app.controller( 'AccountCtrl', function ( $scope )
+{
+	$scope.settings = {
+		enableFriends: true
+	};
+} );
+
