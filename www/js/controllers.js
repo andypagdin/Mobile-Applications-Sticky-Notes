fddce18028d4db386cd93a42871a4b56d37e6a63
Jonathon Hawkins <jonathon.hawkins@conkergroup.com>
2016-11-14 17:15:11 +0000

    updated post comment function simplifying output and cleaning up logic within the Firebase request.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -28,70 +28,6 @@ app = angular.module( 'starter.controllers', [ ] )
 
 
 //////////////////////////////////
-//ChatsCtrl start
-//////////////////////////////////
-
-app.controller( 'ChatsCtrl', function ( $scope, Chats )
-{
-	// With the new view caching in Ionic, Controllers are only called
-	// when they are recreated or on app start, instead of every page change.
-	// To listen for when this page is active (for example, to refresh data),
-	// listen for the $ionicView.enter event:
-	//
-	//$scope.$on('$ionicView.enter', function(e) {
-	//});
-
-	$scope.chats = Chats.all( );
-	$scope.remove = function ( chat )
-	{
-		Chats.remove( chat );
-	};
-} )
-
-//////////////////////////////////
-//ChatsCtrl end
-//////////////////////////////////
-
-
-//////////////////////////////////
-//ChatDetailCtrl start
-//////////////////////////////////
-
-app.controller( 'ChatDetailCtrl', function ( $scope, $stateParams, Chats )
-{
-	$scope.chat = Chats.get( $stateParams.chatId );
-} )
-
-//////////////////////////////////
-//ChatDetailCtrl end
-//////////////////////////////////
-
-
-//////////////////////////////////
-//DashCtrl start
-//////////////////////////////////
-
-app.controller( 'DashCtrl', function ( $scope )
-{
-	firebase.auth( ).onAuthStateChanged( function ( user )
-	{
-		if ( user )
-		{
-			$scope.displayName = user.displayName
-		}
-		else
-		{
-
-		}
-	} );
-} );
-
-//////////////////////////////////
-//DashCtrl end
-//////////////////////////////////
-
-
-//////////////////////////////////
 //AccountCtrl start
 //////////////////////////////////
 
@@ -148,66 +84,113 @@ app.controller( 'AccountCtrl', function ( $scope, $state )
 //FlipCtrl start
 //////////////////////////////////
 
-app.controller( 'FlipCtrl', function ( $scope, Firebase )
-{
-	//this would be set by auth instead of hard coded
-	uuid = 'Ucg7kUNTceQjOLqDIwByHLz5FPj2'
-	$scope.groups = {}
-	$scope.comment_data = {
-		pad_id: "",
-		comment: "",
-		user_id: uuid
-	}
-	console.log( $scope.comment_data )
-	Firebase.get_user_groups( uuid ).then( function ( data )
-	{
-		group_ids = data.val( )
-		Firebase.get_groups( group_ids ).then( function ( groups )
-		{
-			groups.forEach( function ( group, key )
-			{
-				$scope.groups[ group.val( ).group_id ] = group.val( )
-			} );
-			//just to get len -.-
-			angular.forEach( $scope.groups, function ( group_obj, group_key )
-			{
-				angular.forEach( group_obj.pads, function ( pad_obj )
-				{
-					pad_obj.comments.length = Object.keys( pad_obj.comments ).length
-					pad_obj.new_comment = ""
-				} );
-			} );
-			console.info( $scope.groups )
-			console.info( $scope.avalible_pads )
-		} )
-	} )
-	$scope.trigger_comment = function ( element )
-	{
-		var output = {
-			user_id: uuid,
-			sent: Date.now( ),
-			group_id: element.$parent.group_key,
-			pad_id: element.pad_key,
-			comment: element.pad.new_comment
-		}
-		var comment_data = $scope.groups
-		Firebase.post_comment( output )
-	}
+app.controller('FlipCtrl', function($scope, Firebase) {
+    //this would be set by auth instead of hard coded
+    uuid = 'Ucg7kUNTceQjOLqDIwByHLz5FPj2'
+    $scope.groups = {}
+    $scope.comment_data = {
+        pad_id: "",
+        comment: "",
+        user_id: uuid
+    }
+    console.log($scope.comment_data)
+    Firebase.get_user_groups(uuid).then(function(data) {
+        group_ids = data.val()
+        console.log("group_ids", group_ids)
+        Firebase.get_groups(group_ids)
+            .then(function(groups) {
+                $scope.groups = groups
+                console.log("$scope.groups", $scope.groups)
+            })
+    })
+    $scope.trigger_comment = function(element) {
+        var output = {
+            user_id: uuid,
+            sent: Date.now(),
+            group_id: element.$parent.group_key,
+            pad_id: element.pad_key,
+            comment: element.pad.new_comment
+        }
+        var comment_data = $scope.groups
+        Firebase.post_comment(output)
+    }
 
-	$scope.flip = function ( group_key, pad_key )
-	{
-		if ( typeof ( $scope.groups[ group_key ].pads[ pad_key ].flipped ) === "undefeined" )
-		{
-			$scope.groups[ group_key ].pads[ pad_key ].flipped = false;
-		}
-		$scope.groups[ group_key ].pads[ pad_key ].flipped = !$scope.groups[ group_key ].pads[ pad_key ].flipped
-	}
+    $scope.flip = function(group_key, pad_key) {
+        if (typeof ($scope.groups[group_key].pads[pad_key].flipped) === "undefeined") {
+            $scope.groups[group_key].pads[pad_key].flipped = false;
+        }
+        $scope.groups[group_key].pads[pad_key].flipped = !$scope.groups[group_key].pads[pad_key].flipped
+    }
 
 
-} );
+});
 
 //////////////////////////////////
 //FlipCtrl end
+//////////////////////////////////
+
+
+//////////////////////////////////
+//DashCtrl start
+//////////////////////////////////
+
+app.controller( 'DashCtrl', function ( $scope )
+{
+	firebase.auth( ).onAuthStateChanged( function ( user )
+	{
+		if ( user )
+		{
+			$scope.displayName = user.displayName
+		}
+		else
+		{
+
+		}
+	} );
+} );
+
+//////////////////////////////////
+//DashCtrl end
+//////////////////////////////////
+
+
+//////////////////////////////////
+//ChatDetailCtrl start
+//////////////////////////////////
+
+app.controller( 'ChatDetailCtrl', function ( $scope, $stateParams, Chats )
+{
+	$scope.chat = Chats.get( $stateParams.chatId );
+} )
+
+//////////////////////////////////
+//ChatDetailCtrl end
+//////////////////////////////////
+
+
+//////////////////////////////////
+//ChatsCtrl start
+//////////////////////////////////
+
+app.controller( 'ChatsCtrl', function ( $scope, Chats )
+{
+	// With the new view caching in Ionic, Controllers are only called
+	// when they are recreated or on app start, instead of every page change.
+	// To listen for when this page is active (for example, to refresh data),
+	// listen for the $ionicView.enter event:
+	//
+	//$scope.$on('$ionicView.enter', function(e) {
+	//});
+
+	$scope.chats = Chats.all( );
+	$scope.remove = function ( chat )
+	{
+		Chats.remove( chat );
+	};
+} )
+
+//////////////////////////////////
+//ChatsCtrl end
 //////////////////////////////////
 
 
--- www/js/services.js
+++ www/js/services.js
@@ -98,80 +98,89 @@ app.factory( 'Chats', function ( )
 //firebaseServ start
 //////////////////////////////////
 
-app.factory( 'Firebase', function ( $q )
-{
-	get_user_groups = function ( uuid )
-	{
-		console.info( "get_user_groups", uuid )
-		return firebase.database( ).ref( `/users/${uuid}/groups` ).once( 'value', function ( data )
-		{
+app.factory('Firebase', function ($q) {
+	get_user_groups = function (uuid) {
+		console.info("get_user_groups", uuid)
+		return firebase.database().ref(`/users/${uuid}/groups`).once('value', function (data) {
 			return data;
-		} )
+		})
 	}
-	get_groups = function ( group_ids )
-	{
-		// counter = 0
-		console.info( "get_groups", group_ids )
-		groups = [ ]
-		angular.forEach( group_ids, function ( group_object, group_id )
-		{
-			if ( !group_object.read )
-			{
-				groups.push( firebase.database( ).ref( `/groups/${group_id}` ).once( "value", function ( data )
-				{
-					return data.val( )
-				} ) );
-			}
-		} );
-		return $q.all( groups );
+	get_groups = function (group_ids) {
+		promise = []
+		keys = Object.keys(group_ids)
+		for (var i = 0; i < keys.length; i++) {
+			group_id = group_ids[keys[i]]
+			read = group_id.read
+			current_group_id = keys[i]
+			promise.push($q(function (resolve, reject) {
+				if (!read) {
+					return firebase.database().ref(`/groups/${current_group_id}`).once("value", function (groups) {
+						resolve(groups.val());
+					}).catch(reject);
+				}
+				else {
+					resolve();
+				}
+			}))
+		}
+		return $q.all(promise).then(function (outcome) {
+			outcome_obj = {}
+			//clears out all undefined outputs and turns it into an object
+			for (var i = 0; i < outcome.length; i++) {
+				if (outcome[i]) {
+					outcome_obj[i] = outcome[i]
+				}
+			};
+			//set lengths of each sub object
+			outcome_obj.length = Object.keys(outcome_obj).length
+			for (var i = 0; i < outcome_obj.length; i++) {
+				users = outcome_obj[i].users
+				users.length = Object.keys(users).length
+				pads = outcome_obj[i].pads
+				pad_keys = Object.keys(pads)
+				pads.length = Object.keys(pads).length
+				for (var x = 0; x < pad_keys.length; x++) {
+					pad_obj = pads[pad_keys[x]]
+					pad_obj.new_comment = ""
+					comments = pad_obj.comments
+					comments.length = Object.keys(comments).length
+				};
+			};
+			return outcome_obj;
+		}, function (reason) {
+			console.log('Failed: ', reason);
+			return reason;
+		});
 	}
-	post_comment = function ( arg )
-	{
-		console.info( "args", arg )
-		arg.user_id
-		arg.sent
-		arg.group_id
-		arg.pad_id
-		arg.comment
-		arg.comment_id = 123
-
-		comment_output = {
+	post_comment = function (arg) {
+		var ref = firebase.database().ref(`/groups/${arg.group_id}/pads/${arg.pad_id}/comments/`);
+		var comment_id = ref.push().key;
+		var comment_output = {
 			content: arg.comment,
-			comment_id: String( arg.sent ),
+			comment_id: comment_id,
 			sent: arg.sent,
 			user_id: arg.user_id
 		}
-		console.info( "comment_output", comment_output )
 
-
-		set = firebase.database( ).ref( `/groups/${arg.group_id}/pads/${arg.pad_id}/comments/${comment_output.user_id}` ).set( comment_output );
-
-		console.info( "set", set )
-
-		firebase.database( ).ref( `/groups/${arg.group_id}/pads/${arg.pad_id}/comments` ).once( "value", function ( data )
-			{
-				console.log( data.val( ) )
-			} )
-			// firebase.database( ).ref( `/groups/${group_id}` ).set(
-			// {
-			// 	username: name,
-			// 	email: email,
-			// 	profile_picture: imageUrl
-			// } );
+		// Write the session to the database
+		ref.child(`${comment_id}`).set(comment_output).then(function () {
+			// Finally, finish with session information
+			// return comment_output;
+		}).catch(function (error) {
+			console.error("error", error)
+		});
 	}
-	flat_stub = function ( )
-	{
-		return firebase.database( ).ref( "" ).once( 'value', function ( data )
-		{
+	flat_stub = function () {
+		return firebase.database().ref("").once('value', function (data) {
 			return data;
-		} )
+		})
 	}
 	return {
 		get_user_groups: get_user_groups,
 		get_groups: get_groups,
 		post_comment: post_comment
 	};
-} );
+});
 
 //////////////////////////////////
 //firebaseServ end
