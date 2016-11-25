f5d1040fab52aada19debae77d06c338128049d2
Jonathon Hawkins <jonathon.hawkins@conkergroup.com>
2016-11-25 00:24:58 +0000

    posts for comments,pads, groups, user_groups, user_details and logic for setting up new instances of previouse list should now be working if not its becuase im programming at 00:24...

--- www/js/controllers/flipcard/main.js
+++ www/js/controllers/flipcard/main.js
@@ -2,36 +2,104 @@
 //FlipCtrl start
 //////////////////////////////////
 app.controller('FlipCtrl', function ($scope, Firebase) {
-    //this would be set by auth instead of hard coded
-    uuid = 'Ucg7kUNTceQjOLqDIwByHLz5FPj2'
+
     $scope.page_data = {}
     $scope.page_data.groups = {}
+    $scope.page_data.group_models = {}
+    $scope.page_data.pad_models = {}
     $scope.page_data.comment_models = {}
     var group_ids = {}
 
-    Firebase.get_user(uuid).then(function (data) {
-        user_details = data.val()
-        $scope.get_groups(user_details.groups)
+    Firebase.get_user_groups().then(function (outcome) {
+        if (outcome.groups) {
+            console.log("groups", outcome)
+            $scope.get_groups(outcome)
+        }
+        else {
+            console.log("users", outcome)
+            input.groups = {
+                general: true,
+            }
+            Firebase.create_user(outcome)
+        }
     })
-    $scope.get_groups = function (group_ids) {
-        Firebase.get_groups(group_ids)
+    $scope.get_groups = function (input) {
+        Firebase.get_groups(input.groups)
             .then(function (groups) {
                 $scope.page_data.groups = groups
+                $scope.page_data.uid = input.uid
             })
     }
-    $scope.trigger_comment = function (group_id, pad_id) {
+    $scope.create_group = function () {
+        var title = $scope.page_data.group_models.title
+        var input = {
+            title: title,
+        }
+        $scope.page_data.group_models.title = "";
+        Firebase.post_group(input)
+            .then(function (new_object) {
+                group_object = $scope.page_data.groups
+                if (!group_object) {
+                    group_object = {}
+                    group_object.length = 0
+                }
+                group_object[new_object.id] = {}
+                group_object[new_object.id] = new_object
+                group_object.length++
+                console.log("group_object", group_object)
+                if (!$scope.$$phase) {
+                    $scope.$apply()
+                }
+            })
+    }
+    $scope.create_pad = function (group_id) {
+        var title = $scope.page_data.pad_models[group_id].title
+        var body = $scope.page_data.pad_models[group_id].body
+        var input = {
+            group_id: group_id,
+            title: title,
+            body: body,
+        }
+        $scope.page_data.pad_models[group_id].title = "";
+        $scope.page_data.pad_models[group_id].body = "";
+        Firebase.post_pad(input)
+            .then(function (new_object) {
+                pads_object = $scope.page_data.groups[group_id].pads
+                if (!pads_object) {
+                    pads_object = {}
+                    pads_object.length = 0
+                }
+                pads_object[new_object.id] = {}
+                pads_object[new_object.id] = new_object
+                pads_object.length++
+                console.log("pads_object", pads_object)
+                if (!$scope.$$phase) {
+                    $scope.$apply()
+                }
+            })
+    }
+    $scope.create_comment = function (group_id, pad_id) {
+        var body = $scope.page_data.comment_models[pad_id].body
         var input = {
             group_id: group_id,
             pad_id: pad_id,
-            body: $scope.page_data.comment_models[pad_id].body
+            body: body,
         }
         $scope.page_data.comment_models[pad_id].body = "";
         Firebase.post_comment(input)
-            .then(function (new_comment) {
-                comments_object = $scope.page_data.groups[input.group_id].pads[input.pad_id].comments_object
-                comments_object[output.id] = new_comment
+            .then(function (new_object) {
+                comments_object = $scope.page_data.groups[input.group_id].pads[input.pad_id].comments
+                if (!comments_object) {
+                    comments_object = {}
+                    comments_object.length = 0
+                }
+                comments_object[new_object.id]
+                comments_object[new_object.id] = new_object
                 comments_object.length++
-                $scope.$apply()
+                console.log("comments_object", comments_object)
+                if (!$scope.$$phase) {
+                    $scope.$apply()
+                }
             })
     }
 
--- www/js/services/firebase/main.js
+++ www/js/services/firebase/main.js
@@ -3,14 +3,75 @@
 //////////////////////////////////
 
 app.factory('Firebase', function ($q) {
-	get_user = function (uuid) {
-		return firebase.database().ref('/users/').child(uuid).once('value', function (data) {
-			console.log(uuid, data.val())
-			return data;
+	current_user = {}
+	uid = function (uid) {
+		return $q(function (resolve, reject) {
+			if (Object.keys(current_user).length < 1) {
+				firebase.auth().onAuthStateChanged(function (user) {
+					if (user) {
+						current_user = user
+						if (uid) {
+							resolve(user.uid)
+						}
+						resolve(user)
+					}
+					else {
+						reject()
+					}
+				});
+			}
+			else {
+				if (uid) {
+					console.log("cu", current_user.uid)
+					resolve(current_user.uid)
+				}
+				resolve(current_user)
+			}
 		})
+	}
+	get_user_groups = function () {
+		return uid().then(function (user) {
+			var url = `/users/${user.uid}/groups/`
+			console.log("url", url)
+			return firebase.database().ref(url).once('value', function (data) {
+				return data.val()
+			}).then(function (data) {
+				if (data.val()) {
+					console.log("groups")
+					return output = {
+						groups: data.val(),
+						uid:user.uid,
+					}
+				}
+				console.log("output")
+				return output = {
+					displayName: user.displayName,
+					email: user.email,
+					photoURL: user.photoURL,
+					uid: user.uid,
+				}
+			})
+		})
+	}
+	create_user = function (user) {
+		console.log("user", user)
+		var ref = firebase.database().ref('/users');
+		return ref.child(user.uid).set(user).then(function () {
+			return user;
+		}).catch(function (error) {
+			console.error("error", error)
+		});
 	}
 	get_groups = function (group_ids) {
 		promise = []
+		if (!group_ids) {
+			promise.push($q(function () {
+				return {};
+			}))
+			return $q.all(promise).then(function (outcome) {
+				return outcome;
+			})
+		}
 		keys = Object.keys(group_ids)
 		for (var i = 0; i < keys.length; i++) {
 			group_id = group_ids[keys[i]]
@@ -40,17 +101,19 @@ app.factory('Firebase', function ($q) {
 			outcome_obj_keys = Object.keys(outcome_obj)
 			for (var i = 0; i < outcome_obj.length; i++) {
 				outcome = outcome_obj[outcome_obj_keys[i]]
-				users = outcome.users
-				users.length = Object.keys(users).length
 				pads = outcome.pads
-				pad_keys = Object.keys(pads)
-				pads.length = Object.keys(pads).length
-				for (var x = 0; x < pads.length; x++) {
-					pad_obj = pads[pad_keys[x]]
-					pad_obj.new_comment = ""
-					comments = pad_obj.comments
-					comments.length = Object.keys(comments).length
-				};
+				if (pads) {
+					pad_keys = Object.keys(pads)
+					pads.length = Object.keys(pads).length
+					for (var x = 0; x < pads.length; x++) {
+						pad_obj = pads[pad_keys[x]]
+						pad_obj.new_comment = ""
+						comments = pad_obj.comments
+						if (comments) {
+							comments.length = Object.keys(comments).length
+						}
+					};
+				}
 			};
 			return outcome_obj;
 		}, function (reason) {
@@ -58,62 +121,123 @@ app.factory('Firebase', function ($q) {
 			return reason;
 		});
 	}
-	post = function(arg){
-		var ref = firebase.database().ref(arg.url);
+
+	post = function (arg) {
+		var url = arg.url
+		var ref = firebase.database().ref(url);
 		var id = ref.push().key;
-		output = arg.output
+		var output = arg.output
 		output.id = id
 		output.timestamp = Date.now()
-		// call storage to get uuid here.
-	    uuid = 'Ucg7kUNTceQjOLqDIwByHLz5FPj2'
-		output.created_by = uuid
-		console.log("[post][output]",output)
 		return ref.child(id).set(output).then(function () {
 			return output;
 		}).catch(function (error) {
 			console.error("error", error)
 		});
 	}
+	add_user_group = function (arg) {
+		console.log("[add_user_group][arg]", arg)
+		var url = arg.url
+		var ref = firebase.database().ref(url);
+		var group_id = arg.group_id
+		return ref.child(group_id).set(true).then(function () {
+			return
+		}).catch(function (error) {
+			console.error("error", error)
+		});
+	}
+
 	post_group = function (arg) {
-		console.log("[post_group][arg]",arg)
-		var data = {
-			url: `/groups/`,
-			output:{
-				title: arg.title
+		console.log("[post_group][arg]", arg)
+		return uid(true).then(function (uid) {
+			var data = {
+				url: `/groups/`,
+				output: {
+					created_by: uid,
+					title: arg.title,
+					pads: {},
+					users: {},
+				},
 			}
-		}
-		console.log("[post_group][data]",data)
-		return post(data)
+			data.output.users[uid] = true
+			console.log("[post_group][uid]", uid)
+			console.log("[post_group][data]", data)
+			return post(data)
+				.then(function (arg) {
+					console.log("[post return][arg]", arg)
+					var data = {
+						url: `users/${arg.created_by}/groups/`,
+						group_id: arg.id,
+					}
+					add_user_group(data)
+					return arg;
+				})
+				.then(function (output) {
+					console.log("!ouput!", output)
+					var new_object = {
+						id: output.id,
+						title: output.title,
+						timestamp: output.timestamp,
+						pads: {
+							length: 0
+						},
+						users: {
+						},
+					}
+					new_object.users[output.created_by] = true;
+					return new_object;
+				})
+		})
 	}
 	post_pad = function (arg) {
-		console.log("[post_pad][arg]",arg)
-		var data = {
-			url: `/groups/${arg.group_id}/pads/`,
-			output:{
-				title: arg.title,
-				body: arg.body
+		console.log("[post_pad][arg]", arg)
+		return uid(true).then(function (uid) {
+			var data = {
+				url: `/groups/${arg.group_id}/pads/`,
+				output: {
+					created_by: uid,
+					title: arg.title,
+					body: arg.body,
+				},
 			}
-		}
-		console.log("[post_pad][data]",data)
-		return post(data)
+			console.log("[post_pad][data]", data)
+			return post(data)
+				.then(function (output) {
+					var new_object = {
+						id: output.id,
+						title: output.title,
+						body: output.body,
+						timestamp: output.timestamp,
+						created_by: output.created_by,
+						comments: {
+							length: 0
+						},
+					}
+					return new_object;
+				})
+		})
 	}
 	post_comment = function (arg) {
-		console.log("[post_comment][arg]",arg)
-		var data = {
-			url: `/groups/${arg.group_id}/pads/${arg.pad_id}/comments/`,
-			output:{
-				body: arg.body
+		console.log("[post_comment][arg]", arg)
+		return uid(true).then(function (uid) {
+			var data = {
+				url: `/groups/${arg.group_id}/pads/${arg.pad_id}/comments/`,
+				output: {
+					created_by: uid,
+					body: arg.body,
+				}
 			}
-		}
-		console.log("[post_comment][data]",data)
-		return post(data).then(function(){
-			var new_comment = {
-				id: output.id,
-				body: output.body,
-				timestamp: output.timestamp,
-				created_by: output.created_by,
-			}
-			return new_comment;
+			console.log("[post_comment][data]", data)
+			return post(data)
+				.then(function (output) {
+					var new_object = {
+						id: output.id,
+						body: output.body,
+						timestamp: output.timestamp,
+						created_by: output.created_by,
+					}
+					return new_object;
+				})
 		})
 	}
 	flat_stub = function () {
@@ -122,11 +246,13 @@ app.factory('Firebase', function ($q) {
 		})
 	}
 	return {
-		get_user: get_user,
+		uid: uid,
+		create_user: create_user,
+		get_user_groups: get_user_groups,
 		get_groups: get_groups,
 		post_group: post_group,
 		post_pad: post_pad,
-		post_comment: post_comment
+		post_comment: post_comment,
 	};
 });
 
