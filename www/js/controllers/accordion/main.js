22d8744564838c3c23063f59ada6f4338539735f
Andy <andypagdin@Andys-MacBook-Pro-2.local>
2016-12-05 13:54:07 +0000

    Moving accordion to initialise on main dash + delete buttons
    - Moved accordion to initialise on main dash
    - Swipe left shows delete button

--- www/js/controllers/accordion/main.js
+++ www/js/controllers/accordion/main.js
@@ -4,53 +4,16 @@
 
 app.controller('AccordionCtrl', function ($scope, Firebase) {
 
-  // $scope.page_data.groups = [];
-  // for (var i = 0; i < 20; i++) {
-  //   $scope.page_data.groups[i] = {
-  //     name: i,
-  //     items: []
-  //   };
-  //   for (var j = 0; j < 3; j++) {
-  //     $scope.page_data.groups[i].items.push(i + '-' + j);
-  //   }
-  // }
   $scope.page_data = {}
   $scope.page_data.groups = {}
   $scope.page_data.comment_models = {}
   var group_ids = {}
-
-  // Firebase.uid(true) gets uid
-  // Firebase.uid() gets full user object
-  // uid = Firebase.uid(true)
-
-  // check user doesnt need a uid
-  // Firebase.check_user(uid).then(function (data) {
-
-    // user_details = data
-    // console.log("user_details", user_details)
-
-    // This wont work check user isnt in $scope its in Firebase
-    // $scope/Firebase are like an objects but are at the same level
-    // {$scope:{},Firebase:{}}
-    // $scope.check_user(user_details.groups)
-
-  // })
-
 
   Firebase.check_user().then(function (data) {
     user_details = data
     $scope.get_groups(data)
     console.log("user_details", user_details)
   })
-
-  /*$scope.get_user_groups = function (group_ids) {
-    console.log("group_ids", group_ids)
-    Firebase.check_user(group_ids)
-      .then(function (groups) {
-        console.log("groups", groups)
-        $scope.page_data.groups = groups
-      })
-  }*/
 
   $scope.get_groups = function (input) {
     // trigger Firebase function.
@@ -61,7 +24,6 @@ app.controller('AccordionCtrl', function ($scope, Firebase) {
             $scope.groups = groups
         })
     }
-
 
   //togglestar
   $scope.toggleStar = function (group) {
--- www/js/controllers/dashboard/main.js
+++ www/js/controllers/dashboard/main.js
@@ -2,7 +2,8 @@
 //DashCtrl start
 //////////////////////////////////
 
-app.controller('DashCtrl', function ($scope, $state) {
+app.controller('DashCtrl', function ($scope, $state, Firebase) {
+
 	firebase.auth().onAuthStateChanged(function (user) {
 		if (user) {
 			$scope.signed_out = false
@@ -13,6 +14,7 @@ app.controller('DashCtrl', function ($scope, $state) {
 			$scope.signed_out = true
 		}
 	});
+
 	$scope.sign_out = function () {
 		firebase.auth().signOut().then(function () {
 			console.log('Signed Out');
@@ -20,7 +22,62 @@ app.controller('DashCtrl', function ($scope, $state) {
 			console.error('Sign Out Error', error);
 		});
 	}
-});
+
+	//////////////////////////////////
+	// Accordion Controller Underneath
+	//////////////////////////////////
+
+	  $scope.page_data = {}
+	  $scope.page_data.groups = {}
+	  $scope.page_data.comment_models = {}
+	  var group_ids = {}
+
+	  Firebase.check_user().then(function (data) {
+	    user_details = data
+	    $scope.get_groups(data)
+	    console.log("user_details", user_details)
+	  })
+
+	  $scope.get_groups = function (input) {
+	    // trigger Firebase function.
+	    Firebase.get_groups(input.groups)
+	        .then(function (groups) {
+	            console.info("input", input)
+	            console.info("groups", groups)
+	            $scope.groups = groups
+	        })
+	    }
+
+	  //togglestar
+	  $scope.toggleStar = function (group) {
+	    group.star = !group.star;
+	  }
+	  //delete function
+	  $scope.onItemDelete = function (group) {
+	    delete $scope.groups[group]
+	  }
+	  //ReOrder function
+	  $scope.moveItem = function (group, fromIndex, toIndex) {
+	    $scope.groups.splice(fromIndex, 1);
+	    $scope.groups.splice(toIndex, 0, group);
+
+	  };
+	  /*
+	   * if given group is the selected group, deselect it
+	   * else, select the given group
+	   */
+	  $scope.toggleGroup = function (group) {
+	    if ($scope.isGroupShown(group)) {
+	      $scope.shownGroup = null;
+	    } else {
+	      $scope.shownGroup = group;
+	    }
+	  };
+	  $scope.isGroupShown = function (group) {
+	    return $scope.shownGroup === group;
+	  };
+	});
+
 
 //////////////////////////////////
 //DashCtrl end
