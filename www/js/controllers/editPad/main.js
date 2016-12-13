3d121af5522aa901088643b082fee87d0b18f2f9
andypagdin <andyjpagdin@gmail.com>
2016-12-13 21:02:22 +0000

    Update notes

--- www/js/controllers/editPad/main.js
+++ www/js/controllers/editPad/main.js
@@ -4,18 +4,26 @@
 
 app.controller( 'EditPadCtrl', function ( $scope, $state, Firebase, $stateParams)
 {
+	// Put state parameters into the scope
 	$scope.pad_id = $stateParams.pad_id
 	$scope.title = $stateParams.title
 	$scope.body = $stateParams.body
 	$scope.group_id = $stateParams.group_id
+	$scope.created_by = $stateParams.created_by
+	$scope.timestamp = $stateParams.timestamp
 
 	$scope.goBack = function () {
 		$state.go("tab.dash")
 	}
 
-	// Not working yet
-	$scope.updateNote = function ($body) {
-		console.log($body)
+	$scope.updateNote = function ( $body ) {
+		firebase.database().ref('groups/' + $scope.group_id + '/pads/' + $scope.pad_id).set({
+			body: $body,
+			created_by: $scope.created_by,
+			id: $scope.pad_id,
+			timestamp: $scope.timestamp,
+			title: $scope.title
+		})
 	}
 
 } );
