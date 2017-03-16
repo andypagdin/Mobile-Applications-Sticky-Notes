881b9289334deeb9e41c926143e4924dce7190cc
Jonathon Hawkins <jonathon.hawkins@conkergroup.com>
2017-03-16 20:33:09 +0000

    switches are more efficient than ifs.

--- www/js/controllers/head/main.js
+++ www/js/controllers/head/main.js
@@ -1,26 +1,21 @@
 app.controller( 'headCtrl', function ( $scope, Css) {
-
 	$scope.cssOptions = [
 	    { name: 'Default', value: 'style' },
 	    { name: 'Dark', value: 'dark-style' },
 	    { name: 'Colour Blind', value: 'assisted-style' }
 	];
-
 	$scope.Css = Css;
-
    	$scope.changePath = function() {
-   		if($scope.css == "style"){
-   			Css.setUrl('css/style');
-   		}
-   		else if($scope.css == "dark-style")
-   		{
-   			Css.setUrl('css/dark-style');
-   		}
-   		else if($scope.css == "assisted-style")
-   		{
-   			Css.setUrl('css/assisted-style');
-   		}
-   		//console.log($scope.css)
+		switch($scope.css){
+			case"style":
+				Css.setUrl('css/style');
+			break;
+			case "assisted-style":
+				Css.setUrl('css/assisted-style');
+			break;
+			default:
+				Css.setUrl('css/dark-style');
+			break;
+		}
   	};
-
 } )
