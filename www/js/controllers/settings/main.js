5646fc13a1772d47e9c6384e328c3694967b2b7c
Jonathon Hawkins <jonathon.hawkins@conkergroup.com>
2017-03-16 21:03:20 +0000

    more git testing.

--- www/js/controllers/settings/main.js
+++ www/js/controllers/settings/main.js
@@ -4,10 +4,8 @@
 
 app.controller( 'SettingsCtrl', function ( $scope, FirebaseServ, $timeout, $state )
 {
-
     var app = angular.module( 'myApp', [ ] );
     $scope.today = new Date( );
-
     $scope.signOut = function ( )
     {
         firebase.auth( ).signOut( ).then( function ( )
